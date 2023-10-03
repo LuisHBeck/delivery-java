ae8e1481c14a94601e175c856d3cbb6160cee911
LuisHBeck <beck.henrique.luis@gmail.com>
2023-10-03 11:14:48 -0300

    feat: sandwich list screen

---
+++ src/entities/Screens/SandwichListScreen.java
@@ -0,0 +1,11 @@
+package entities.Screens;
+
+import javax.swing.table.DefaultTableModel;
+
+public class SandwichListScreen extends Screen{
+    public SandwichListScreen(Boolean visibility) {
+        super(visibility);
+    }
+
+    
+}
